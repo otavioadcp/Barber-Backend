fdaff6ae158c891ae5b1bfe49f3646497d7ab715
Otavio Pimentel <otaviopimentel@Otavios-MacBook-Pro.local>
2020-05-19 15:32:34 -0400

    Criandoo relacao MtO de User e Appointment

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,12 +1,16 @@
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm'
-
-@Entity('appointments')
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn } from 'typeorm'
+import User from './User'
+@Entity('appointments') 
 class Appointment {
     @PrimaryGeneratedColumn('uuid')
     id: string;
     
     @Column()
     provider_id: string;
+
+    @ManyToOne(() => User)
+    @JoinColumn({ name: 'provider_id'})
+    provider: User;
 
     @Column('timestamp with time zone')
     date: Date;
